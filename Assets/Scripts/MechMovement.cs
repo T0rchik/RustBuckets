2a281c2e39858ec50d0edd6317b81eb9d4c468c7
Michael Dardour <michael.dardour@gmail.com>
2020-08-09 19:33:53 -0400

    Basic Movement
    Basic Mech movement created, thinking of options for rotating the mech currently.

---
+++ Assets/Scripts/MechMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+using Valve.VR.InteractionSystem;
+
+public class MechMovement : MonoBehaviour
+{
+    public LinearMapping leftControl;
+    private float leftValueCurrent;
+    public LinearMapping rightControl;
+    private float rightValueCurrent;
+    private float difference;
+
+    public Rigidbody mech;
+
+    [Header("Movement Settings")]
+    public float force = 30;
+    public float forceConstant = 5;
+
+
+    private bool movementActive = false;
+
+    private int framesUnchanged;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mech.AddForce(Vector3.forward);
+
+        StartCoroutine(startUp());
+
+        leftValueCurrent = leftControl.value;
+        rightValueCurrent = rightControl.value;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (movementActive && leftValueCurrent != leftControl.value || rightValueCurrent != rightControl.value)
+        {
+            float leftDif = leftValueCurrent - leftControl.value;
+            float rightDif = rightValueCurrent - rightControl.value;
+
+            leftValueCurrent = leftControl.value;
+            rightValueCurrent = rightControl.value;
+
+
+            //difference = (leftValueCurrent / (1 - rightValueCurrent));
+            //if (difference > 1) difference = 2 - difference;
+
+            difference = 0;
+            if (leftDif < 0) difference -= leftDif;
+            if (rightDif < 0) difference -= rightDif;
+
+            difference *= 2000;
+
+            Debug.Log(difference);
+
+            mech.AddRelativeForce(Vector3.forward * (force * difference + forceConstant), ForceMode.Impulse);
+
+            framesUnchanged = 0;
+        }
+        else
+        {
+            framesUnchanged++;
+            if (framesUnchanged > 10)
+            {
+                //mech.velocity = Vector3.zero;
+            }
+        }
+    }
+
+    IEnumerator startUp()
+    {
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForSeconds(1.5f);
+        movementActive = true;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        Instantiate(playerObject, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
