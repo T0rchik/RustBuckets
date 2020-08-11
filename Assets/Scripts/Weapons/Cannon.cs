9b10eddfba5a85a56ab92988e0c9529a3de8a0c3
kac486 <kac486@drexel.edu>
2020-08-11 16:55:48 -0400

    Weapons scripts started

---
+++ Assets/Scripts/Weapons/Cannon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : Weapon
+{
+    public Transform muzzlePoint;
+    public GameObject projectile;
+    public float projectileLifetime = 5.0f;
+    public float shootForce;
+
+    public Cannon(int maxAmmo, float shootTime, int damage) : base(maxAmmo, shootTime, damage)
+    {}
+
+    public void Fire()
+    {
+        if(currAmmo > 0)
+        {
+            currAmmo -= 1;
+            GameObject currProjectile = (GameObject)Instantiate(projectile, muzzlePoint.position, muzzlePoint.rotation);
+            currProjectile.GetComponent<Rigidbody>().AddForce(muzzlePoint.up * shootForce);
+            Destroy(currProjectile, projectileLifetime);
+            timeToShoot = shootTime;
+        } else {
+            Debug.Log("Out of Ammo!");
+            // Add onscreen indicator or something
+        }
+    }
+
+    public void AltFire()
+    {
+        // Zoom in function
+        Debug.Log("View zooms in.");
+    }
+
+    // Cannon is single shot and reloads between
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    protected int maxAmmo;
+    protected int currAmmo;
+    protected float shootTime;
+    protected float timeToShoot;
+    protected int damage;
+
+    public Weapon(int maxAmmo, float shootTime, int damage)
+    {
+        this.maxAmmo = maxAmmo;
+        this.currAmmo = maxAmmo;
+        this.shootTime = shootTime;
+        this.damage = damage;
+    }
+
+    void Start()
+    {
+       timeToShoot = 0.0f; 
+    }
+
+    public void Fire()
+    {
+        // Fires the weapon
+    }
+
+    public void AltFire()
+    {
+        // Performs weapons alternate action
+    }
+
+    public void Reload()
+    {
+        // Reloads the weapon if applicable
+    }
+}
