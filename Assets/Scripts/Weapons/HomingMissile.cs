650c1a289344376d7c3f3285b117d075ce70a4bf
kac486 <kac486@drexel.edu>
2020-09-01 17:38:07 -0400

    Added missing Missile

---
+++ Assets/Scripts/Weapons/HomingMissile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class HomingMissile : MonoBehaviour
+{
+    public float turn;
+    public float missileVelocity;
+
+    Transform target;
+    bool hasTarget = false;
+
+    void Update()
+    {
+        if(hasTarget)
+        {
+            Debug.Log("Missile Fired");
+            GetComponent<Rigidbody>().velocity = transform.forward * missileVelocity;
+            Quaternion missileTargetRotation = Quaternion.LookRotation(target.position - transform.position);
+            GetComponent<Rigidbody>().MoveRotation(Quaternion.RotateTowards(transform.rotation, missileTargetRotation, turn));
+        }
+    }
+
+    public void Fire(Transform tgt)
+    {
+        target = tgt;
+        hasTarget = true;
+        Debug.Log("Set Target");
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //Visual Effect Explosion 
+        /*
+        Vector3 hitPosition = transform.position;
+        Quaternion hitRotation = transform.rotation;
+        Destroy(gameObject);
+        VisualEffect explosion = (VisualEffect)Instantiate(explosionEffect, hitPosition, hitRotation);
+        Destroy(explosion, 0.5f);
+        */
+        //Destroy(gameObject);        // Delete after adding explosion effects
+        Debug.Log("Target Hit!");
+    }
+}
