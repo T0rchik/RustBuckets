e8036312187aa8f0e2bfded0c95c5792b4058943
kac486 <kac486@drexel.edu>
2020-09-04 15:31:30 -0400

    Updated Center Monitor to include Missile Launcher, and added controller

---
+++ Assets/Scripts/Monitor/MonitorControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class MonitorControl : MonoBehaviour
+{
+        public HoverButton minimap;
+        public HoverButton missileLauncher;
+        public GameObject monitor;
+
+        Canvas[] screens;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screens = monitor.GetComponentsInChildren<Canvas>();
+        screens[0].enabled = false;
+        screens[1].enabled = false;
+
+        minimap.onButtonDown.AddListener(Minimap);
+        missileLauncher.onButtonDown.AddListener(MissileLauncher);
+    }
+
+    public void Minimap(Hand hand)
+    {
+        if(screens[1].enabled)
+        {
+            screens[1].enabled = false;
+        }
+
+        screens[0].enabled = true;
+
+    }
+
+    public void MissileLauncher(Hand hand)
+    {
+        if(screens[0].enabled)
+        {
+            screens[0].enabled = false;
+        }
+
+        screens[1].enabled = true;
+
+    }
+}
