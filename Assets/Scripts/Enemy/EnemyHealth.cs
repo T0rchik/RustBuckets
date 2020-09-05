95c7c12088f154cf9169a3edd5c21ab04c4a338b
Michael Dardour <michael.dardour@gmail.com>
2020-09-04 20:00:10 -0400

    Basic Enemy death will improve

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int currentHealth = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("playerProjectile"))
+        {
+            Weapon shot = collision.gameObject.GetComponent<WeaponReference>().origin;
+            DealDamage(shot.damage);
+        }
+    }
+
+    public void DealDamage(int damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+
+        Debug.Log("Damage: " + damage);
+
+        currentHealth -= damage;
+
+        CheckYoSelf();
+    }
+
+    private void CheckYoSelf()
+    {
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
